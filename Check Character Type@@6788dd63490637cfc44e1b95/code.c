26f9bfce3fa76fbcacd477a60b4fc45ac0bfaaed
simransharma07 <simransharma3507@gmail.com>
2025-02-15 13:13:11 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -1,19 +1,22 @@
-#include<stdio.h>
-int main()
-{
+#include <stdio.h>
+
+int main() {
     char ch;
-    scanf("%c",&ch);
-    if(ch>='0'&& ch<= '9'){
+    scanf("%c", &ch);
+
+    if (ch >= '0' && ch <= '9') {
         printf("Digit\n");
-    }
-    else if(ch== 'a' || ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'){
+    } 
+    else if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+             ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
         printf("Vowel\n");
+    } 
+    else if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
+        printf("Consonant\n");  // Fixed spelling from "Consonent" to "Consonant"
+    } 
+    else {
+        printf("Special Character\n");  // Added missing semicolon
     }
-    else if((ch>='A'&& ch<='Z')||(ch>='a'&&ch<='z'))
-    {
-        printf("Consonent\n");
-    }
-    else{
-        printf("Special Character\n")
-    }
+
+    return 0;
 }
